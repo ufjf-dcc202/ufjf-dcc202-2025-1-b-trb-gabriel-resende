0b24e9e61514497e27dba8696ffa52a2ce2bdbcc
Gabriel Resende <gabriel.pereira@gazin.com.br>
2025-08-19 20:07:38 -0300

    feat: set click actions

--- scripts.js
+++ scripts.js
@@ -17,3 +17,33 @@ for (let i = 0; i < 144; i++) {
 
   farmGrid.appendChild(cell);
 }
+
+const cells = document.querySelectorAll(".cell");
+const messageBox = document.querySelector(".message-box p");
+const tools = document.querySelectorAll(".tool");
+const items = document.querySelectorAll(".shop");
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    if (cell.classList.contains("rock") || cell.classList.contains("weed")) {
+      cell.classList.remove("rock", "weed");
+      cell.textContent = "";
+      messageBox.textContent =
+        "Bom trabalho! Continue limpando o terreno. 50/144 limpos.";
+    }
+  });
+});
+
+tools.forEach((tool) => {
+  tool.addEventListener("click", () => {
+    tools.forEach((t) => t.classList.remove("selected"));
+    tool.classList.add("selected");
+  });
+});
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    items.forEach((i) => i.classList.remove("selected"));
+    item.classList.add("selected");
+  });
+});
